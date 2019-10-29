a61cb648df4cd4003d87c91c4ca6618313001f41
piemekanika <piemekanika@hotmail.com>
2019-10-29 23:29:10 +0700

    started implementing button atom

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="
+      ca-button border-2 
+      rounded font-bold 
+      uppercase text-lg 
+      text-indigo-600 
+      border-indigo-600 
+      hover:bg-indigo-100
+    "
+    v-on="$listeners"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.ca-button {
+  transition: all .2s ease-in;
+  padding: 10px 30px;
+}
+</style>
