d42813d2b72a6663d9cd0d3204ca7be07e81d8f2
piemekanika <piemekanika@hotmail.com>
2019-11-01 00:44:27 +0700

    fixed code styling

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -1,12 +1,12 @@
 <template>
   <button
     class="
-    ca-button border-2 
-    rounded font-bold 
-    uppercase text-lg 
-    text-indigo-600 
-    border-indigo-600 
-    hover:bg-indigo-100
+      ca-button border-2 
+      rounded font-bold 
+      uppercase text-lg 
+      text-indigo-600 
+      border-indigo-600 
+      hover:bg-indigo-100
     "
     :class="{'ca-button--wide': wide}"
     v-on="$listeners"
