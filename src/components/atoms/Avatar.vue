9fbf361d192a88569396ab3110fa6d88bbc0c106
Valya <49086658+Narfisa@users.noreply.github.com>
2019-11-03 23:06:41 +0700

    fixed indent

--- src/components/atoms/Avatar.vue
+++ src/components/atoms/Avatar.vue
@@ -1,17 +1,17 @@
 <template>
   <img
-  class="Avatar"
+  class="avatar"
   v-bind="$attrs"
   alt="avatar"
   >
 </template>
 
 <style lang="scss" scoped>
-  .Avatar{
-    width: 36px;
-    height: 36px;
-    object-fit: cover;
-    border-radius: 50%;
-    overflow: hidden;
-  }
+.avatar{
+  width: 36px;
+  height: 36px;
+  object-fit: cover;
+  border-radius: 50%;
+  overflow: hidden;
+}
 </style>
