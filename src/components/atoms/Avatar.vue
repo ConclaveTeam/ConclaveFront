514605e5f2ccc2a6be4844dd520c64c1ff26d702
Valya <49086658+Narfisa@users.noreply.github.com>
2019-11-03 12:47:31 +0700

    Created a avatar component

---
+++ src/components/atoms/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <img
+  class="Avatar"
+  :src=imgsrc
+  alt="avatar"
+  >
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Avatar extends Vue{
+  @Prop(String) imgsrc!: string
+}
+</script>
+
+<style lang="scss" scoped>
+  .Avatar{
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
+</style>
