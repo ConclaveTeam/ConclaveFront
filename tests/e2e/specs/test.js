ff59820f10619d5592dcaa1a219a841b2c043c7c
piemekanika <piemekanika@hotmail.com>
2019-11-03 16:44:20 +0700

    fixed test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('http://localhost:8080/test-page')
-    cy.contains('button', 'testing button')
+    cy.contains('button', 'Testing button')
   })
 })
